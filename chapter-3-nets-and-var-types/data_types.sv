e05d24fdaf122600744272c29ceaa780585ad262
Augusto <augusto.gomez@mi.unc.edu.ar>
2024-05-05 18:10:33 -0300

    [CH3] Update chapter 3

--- chapter-3-nets-and-var-types/data_types.sv
+++ chapter-3-nets-and-var-types/data_types.sv
@@ -167,14 +167,118 @@ always_ff @(posedge elk)
     from other keywords and context.
 */
 
-    logic v1; // infers var
-    bit v2; // infers var
-    integer v3; // infers var
-    int v4; // infers var
+    logic v1; // infers var logic (1-bit 4-state)
+    bit v2; // infers var bit (1-bit 2-state)
+    integer v3; // infers var integer (32-bit 4-state)
+    int v4; // infers var int (32-bit 2-state)
 
 /*
     The only place where the var keyword is required is when declaring an input or
     inout port as a 4-state variable. These port directions will default to a net type if not
     explicitly declared as a variable. This is an appropriate default. It is very seldom that
     an input port needs to be a variable.
+*/
+
+/* 
+    Scalar variables
+    
+    A scalar variable is a 1-bit variable. The "reg", "logic" and "bit" data types default to 1-bit scalars.
+*/
+    logic v5; // a 1-bit scalar variable
+    logic v6, v7, v8; // a list of three scalar variables
+
+/* 
+    Vector variables (packed arrays)
+    
+    A vector is an array of consecutive bits. The IEEE SystemVerilog standard refers to vectors as packed arrays. 
+    The reg, logic and bit data types can represent a vector of any size.
+    The range is declared as [most-significant_bit_number : least-significant_bit_number]. 
+    The MSB and LSB can be any number, and the LSB can be smaller or larger than the MSB. A vector range where the LSB is
+    the smaller number is referred to as little endian. A vector range where the LSB is the larger number is referred to 
+    as big endian.
+
+    The byte, shortint, int, longint and integer data types have a predefined vector size. The predefined ranges are little
+    endian, with the LSB numbered as bit 0.
+*/
+    logic [31:0] v9; // 32-bit vector, little endian
+    logic [1:32] v10; // 32-bit vector, big endian
+
+/*
+    Signed and unsigned variables
+    
+    The value stored in a vector variable can be treated as either signed or unsigned in operations. An unsigned variable 
+    only stores positive values. A signed variable can store positive and negative values. SystemVerilog uses two’s-complement 
+    to represent negative values. The most significant bit of a signed variable is the sign bit. When the sign bit is set, 
+    the remaining bits of the vector represent a negative value in two’s-complement form.
+
+    By default, the reg, logic, bit and time data types are unsigned variables, and the byte, shortint, int, integer, and 
+    longint data types are signed variables. This default can be changed by explicitly declaring a variable as signed or
+    unsigned.
+*/
+    logic signed [23:0] vll; // 24-bit signed 4-state variable
+    int unsigned vl2; // 32-bit unsigned 2-state variable
+
+/*
+    Constant bit selects and part selects
+    
+    A vector can be referenced in its entirety, or in part. A bit select references a single bit of a vector. A bit select 
+    is performed using the name of the vector, followed by a bit number in square brackets. A part select references 
+    multiple contiguous bits of a vector. A part select is performed using the name of the vector, followed by a range of 
+    bit numbers in square brackets.
+
+    Part selects must meet two rules: the range of bits must be contiguous, and the endian of the part select must be the
+    same endian as the vector declaration. The result of a bit select or part select is always unsigned, even if the full 
+    variable is signed.
+*/
+    logic [31:0] data;  // 32-bit vector variable
+    logic [31:0] dbus;  // 32-bit vector variable
+    logic [ 7:0] byte2; // 8-bit vector variable
+    logic msb;          // 1-bit scalar variable
+
+    assign dbus = data; // reference entire data variable
+    assign msb = data[31]; // bit select of data, bit 31
+    assign byte2 = data[23:16]; // part select of data bits 23
+                                // down to 16
+
+/*
+    Variable bit and part selects
+    
+    The bit select for msb in the preceding code snippet used a hard-coded bit number. This is referred to as a fixed bit 
+    select. The index number of a bit select can also be a variable.
+*/
+    logic [31:0] data; // 32-bit vector variable
+    logic bit_out; // 1-bit scalar variable
+    
+    always @(posedge shift_clk)
+        if (shift_enable) begin
+            for (int i=0; i<=31; i++) begin
+                @(posedge shift_clk) bit_out <= data[i];
+            end
+        end
+
+/* 
+    The starting point of a part select can also be variable. The part select can either increment or decrement from 
+    the variable starting point. The total number of bits selected is a fixed range. The form of a variable 
+    part select is:
+
+    [ starting_point_variable +: part_select_size ]
+    [ starting_point_variable -: part_select_size ]
+    
+    The +: token indicates to increment from the starting point bit number. The -: token indicates to decrement from 
+    the starting point bit number. The following example uses a variable part select to iterate through the bytes of a
+    32-bit vector.
+*/
+    logic [31:0] data; // 32-bit vector variable
+    logic [ 7:0] byte_out; // 8-bit vector variable
+
+    always @ (posedge shift_clk)
+        if (shift_enable) begin
+            for (int i=0; i<=31; i=i+8) begin
+                @(posedge shift_clk) byte_out <= data[i+:8];
+            end
+        end           
+
+/*
+    Variable bit and part selects are synthesizable. However, the preceding code snippets illustrating variable bit and 
+    part selects do not meet other RTL coding restrictions required by some synthesis compilers.
 */
